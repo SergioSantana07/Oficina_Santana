ed28470a2db64a9b752b730ce29bb858034d83a9
SergioSantana07 <sergiosantanafilho07@gmail.com>
2026-06-28 13:45:24 -0300

    Área administrativa: login e dashboard concluídos

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('servicos/', views.servicos, name='servicos'),
     path('contato/', views.contato, name='contato'),
+    path('admin-santana/login/', views.admin_login, name='admin_login'),
+    path('admin-santana/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin-santana/logout/', views.admin_logout, name='admin_logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +14,23 @@ def servicos(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def admin_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('admin_dashboard')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+    return render(request, 'painel/login.html')
+
+def admin_logout(request):
+    logout(request)
+    return redirect('admin_login')
+
+@login_required(login_url='/admin-santana/login/')
+def admin_dashboard(request):
+    return render(request, 'painel/dashboard.html')
